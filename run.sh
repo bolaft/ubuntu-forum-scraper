6c75c66c5061135d1d363e016e2408b7aa56e545
bolaft <soufi@nsal.im>
2015-01-14 12:36:27 +0100

    merged

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@
 
 # scrapy runspider forum_spider.py -o data/forums.json -s DOWNLOAD_DELAY=3
 # scrapy runspider thread_spider.py -o data/threads.json -s DOWNLOAD_DELAY=3
-scrapy runspider post_spider.py -o data/posts.json -s DOWNLOAD_DELAY=3
+scrapy runspider post_spider.py -o data/posts_2012.json -s DOWNLOAD_DELAY=3
 
-./assembler.py data/map.json
+./assembler.py data/map_2012.json
